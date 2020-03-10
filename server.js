15e31e9460a682a69f80cfd1fe1d95cce0216f3f
Sarah Loach <Sarah@Sarahs-fat-top.local>
2020-03-10 17:27:15 -0500

    created authorization for endpoint and filtering based on string parameters

--- server.js
+++ server.js
@@ -1,13 +1,66 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const MOVIES = require('./movies.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-app.use((req, res) =>{
-    res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+  })
+
+
+  app.get('/movie', function handleGetMovies(req, res) {
+    let response = MOVIES;
+    
+
+    //filter by genre if genre param is present
+    if(req.query.genre) {
+        response = response.filter(movie => {
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        })
+    }
+    
+
+    //filter by country if country param is present
+    if(req.query.country) {
+        response = response.filter(movie => {
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        })
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie => {
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        })
+    }
+
+    //filter by average vote if average vote param is present
+    if(req.query.avg_vote) {
+        response = response.filter(movie => {
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        })
+    }
+
+
+    res.json(response)
 })
+
+
+
 
 const PORT = 8000
 
